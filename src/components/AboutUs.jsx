b30dafbb6d2c79df396b9a42e5d88e7fb944dbec
ArifRahmatullah03 <arifrahmatullah03@students.undip.ac.id>
2026-02-15 13:57:32 +0700

    Perubahan Maps

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,22 +1,22 @@
 const AboutUs = () => {
   return (
     <section id="about">
-      {/* JUDUL */}
+      {/* Header Section */}
       <div className="section-header">
         <h1>Tentang Kami</h1>
       </div>
 
-      {/* MAP */}
+      {/* Map Section */}
       <div className="about-map-full">
         <iframe
           title="Lokasi Garuda Coffee"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d494.0289837405992!2d109.85568474999999!3d-7.0187206!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e70150031badb9d%3A0x665c1e0c45e4edbd!2sGaruda%20Coffee!5e0!3m2!1sen!2sid!4v1234567890"
+          src="https://www.google.com/maps?q=-7.018917,109.855833&z=15&output=embed"
           loading="lazy"
           allowFullScreen
         />
       </div>
 
-      {/* CERITA TANPA SUBJUDUL */}
+      {/* Description Section */}
       <div className="about-description">
         <p>
           Garuda Coffee lahir dari kecintaan terhadap kopi nusantara serta
